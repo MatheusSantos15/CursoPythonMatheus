e9423c4640ca6a59e5a83c12a86c092d3d6a3aa6
Matheus Santos <msantosnascimento1504@gmail.com>
2023-12-25 21:32:18 -0300

    exDuploTriploRaiz

---
+++ exDuploTriploRaiz.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número: '))
+dobro = num*2
+triplo = num*3
+raiz = num**(1/2)
+print('O dobro de {} vale {}.\nO triplo de {} vale {}.\nA raiz quadrada de {} é igual a {:.2f}.'.format(num,dobro,num,triplo,num,raiz))
